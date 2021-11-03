a5391fc12d858e1f7ffca840a62106710966fd73
Ivan Alexander Mendivil Araujo <alexanderIMA@outlook.es>
2021-11-03 13:51:30 -0700

    host especificado y escaneo de 200 puertos

--- port_scanner/port_Scanner.py
+++ port_scanner/port_Scanner.py
@@ -3,20 +3,22 @@
 import socket 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-host = "192.168.56.1"
-port = 445
+socket.setdefaulttimeout(1)
+# 192.168.56.1
+host = input("[*] Enter the host to scan: ")
+# port = int(input("[*] Enter de port to scan: "))
 
 def Scanner(port):
     if sock.connect_ex(( host, port )):
         print(f"Port {port} is closed")
     else:
-        print(f"Port {port} is opened")
+        print(f"Port {port} is open")
 
 
 if __name__ == "__main__":
     try:
-        Scanner(port)
+        for port in range(1,200):
+            Scanner(port)
     except:
         print("No error handling")
 
